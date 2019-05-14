7c445d9c39b45893ceb73888119d326706f9d1a1
umairabid <umairac90@gmail.com>
2019-05-14 23:56:02 +0500

    task detail screen design

--- src/components/MainApp/TaskDetail/TaskDetail.vue
+++ src/components/MainApp/TaskDetail/TaskDetail.vue
@@ -1,5 +1,44 @@
 <template>
-  <section id="task-details-screen" class="dashboard-screen"></section>
+  <section id="task-details-screen" class="dashboard-screen">
+    <div class="content-block task-detail">
+      <h2>This is title of my task</h2>
+      <div class="row">
+        <div class="column">
+          <strong>Due Date:</strong>
+          <span>14 May 2019</span>
+        </div>
+        <div class="column">
+          <strong>Assignee:</strong>
+          <span>Umair Abid</span>
+        </div>
+        <div class="column">
+          <strong>Status:</strong>
+          <span>Open</span>
+        </div>
+      </div>
+      <p>Morbi laoreet augue eu leo imperdiet laoreet. Donec vel efficitur magna. Integer venenatis est eros, a imperdiet mauris ornare in. Phasellus ullamcorper sapien a elit interdum convallis. Sed non sapien at urna rhoncus sollicitudin et nec odio. Proin lacinia hendrerit lorem. Cras nec tempor augue, et ullamcorper orci.</p>
+      <div class="row">
+        <div class="column">
+          <strong>Maximum Plagarism Ratio:</strong>
+          <span>10%</span>
+        </div>
+        <div class="column">
+          <strong>Maximum Mistakes:</strong>
+          <span>10%</span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column">
+          <strong>Payment Type:</strong>
+          <span>Fixed</span>
+        </div>
+        <div class="column">
+          <strong>Payment:</strong>
+          <span>100</span>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script src="./TaskDetail.js"></script>
